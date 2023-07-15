63947c529ac05509fd262bf8eee79dc5c99712bf
MichaelVerthin <86958340+Michael-Verthin@users.noreply.github.com>
2023-07-15 10:23:56 +0300

    Updates to main.js now

--- js/main.js
+++ js/main.js
@@ -9,19 +9,30 @@
         });
     }, { offset: '80%' });
 
+    function animateSkills() {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }
+    document.addEventListener('DOMContentLoaded', function () {
+        var skillContainer = document.querySelector('.skill-container');
+        skillContainer.classList.remove('hidden');
+        animateSkills();
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('.back-to-top').fadeIn('slow');
+            } else {
+                $('.back-to-top').fadeOut('slow');
+            }
+        });
+
+        $('.back-to-top').click(function () {
+            $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+            return false;
+        });
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-        return false;
-    });
+
 
 })(jQuery);
 window.addEventListener('load', () => {
