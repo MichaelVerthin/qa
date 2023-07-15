e69925b367dc9780e867942abdb17639922264e5
MichaelVerthin <86958340+Michael-Verthin@users.noreply.github.com>
2023-07-15 10:32:08 +0300

    Second try to fix the sliders for Skills

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
-
 (function ($) {
     "use strict";
 
     // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, { offset: '80%' });
-
     function animateSkills() {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
+        $('.skill').waypoint(
+            function () {
+                $('.progress .progress-bar').each(function () {
+                    $(this).css("width", $(this).attr("aria-valuenow") + '%');
+                });
+            },
+            { offset: '80%' }
+        );
     }
-    document.addEventListener('DOMContentLoaded', function () {
-        var skillContainer = document.querySelector('.skill-container');
-        skillContainer.classList.remove('hidden');
-        animateSkills();
 
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 200) {
-                $('.back-to-top').fadeIn('slow');
-            } else {
-                $('.back-to-top').fadeOut('slow');
-            }
-        });
-
-        $('.back-to-top').click(function () {
-            $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-            return false;
-        });
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
 
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Call animateSkills when the page finishes loading
+    $(window).on('load', function () {
+        animateSkills();
+    });
 
 })(jQuery);
-window.addEventListener('load', () => {
-    init();
-});
-
